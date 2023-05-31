0514acde9dc4bcdc78b550138d65c5296a37d044
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2023-05-31 10:02:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 // Import required modules
-const axios = require('axios');
-const cron = require('cron');
-const fs = require('fs');
+const axios = require("axios");
+const cron = require("cron");
+const fs = require("fs");
 
 // Load the JSON file
-const jsonData = require('./aptitude.json');
+const jsonData = require("./aptitude.json");
 const questions = jsonData.questions;
 
 // Set up the Discord webhook URL
-const webhookUrl = 'YOUR_DISCORD_WEBHOOK_URL';
+const webhookUrl = "YOUR_DISCORD_WEBHOOK_URL";
+
+// Counter variable to track the current day
+let currentDay = 1;
 
 // Function to send questions to the Discord webhook
 const sendQuestions = () => {
-  const currentDay = sendQuestionsJob.currentTick() + 1;
   const startIdx = (currentDay - 1) * 10;
   const endIdx = currentDay * 10;
 
   const webhookData = {
     embeds: [
       {
-        title: 'Aptitude Questions',
+        title: "Aptitude Questions",
         description: `Here are today's questions (Day ${currentDay}):`,
         color: getColorForDay(currentDay),
         fields: [],
@@ -32,27 +34,28 @@ const sendQuestions = () => {
     const question = questions[i];
     webhookData.embeds[0].fields.push({
       name: `Question ${i + 1}`,
-      value: `${question.question}\n\nGiven: ${question.given}`,
+      value: `${question.question}\n\n**Given**: \`\`\`${question.given}\`\`\`\n==============================\n`,
     });
   }
 
   // Send the webhook request
   axios
     .post(webhookUrl, webhookData)
-    .then(() => console.log('Questions sent successfully!'))
-    .catch((error) => console.error('Error sending questions:', error));
+    .then(() => console.log("Questions sent successfully!"))
+    .catch((error) => console.error("Error sending questions:", error));
+
+  currentDay++; // Increment the current day counter
 };
 
 // Function to send answers to the Discord webhook
 const sendAnswers = () => {
-  const currentDay = sendAnswersJob.currentTick() + 1;
   const startIdx = (currentDay - 1) * 10;
   const endIdx = currentDay * 10;
 
   const webhookData = {
     embeds: [
       {
-        title: 'Aptitude Answers',
+        title: "Aptitude Answers",
         description: `Here are today's answers (Day ${currentDay}):`,
         color: getColorForDay(currentDay),
         fields: [],
@@ -65,23 +68,23 @@ const sendAnswers = () => {
     const question = questions[i];
     webhookData.embeds[0].fields.push({
       name: `Question ${i + 1}`,
-      value: `Answer: ${question.answer}\n\nExplanation: ${question.explanation}\n\nSolution: ${question.solution}`,
+      value: `**Answer**: \`\`\`${question.answer}\`\`\`\n\n**Explanation**: ${question.explanation}\n\n**Solution**: ${question.solution}\n==============================\n`,
     });
   }
 
   // Send the webhook request
   axios
     .post(webhookUrl, webhookData)
-    .then(() => console.log('Answers sent successfully!'))
-    .catch((error) => console.error('Error sending answers:', error));
+    .then(() => console.log("Answers sent successfully!"))
+    .catch((error) => console.error("Error sending answers:", error));
 };
 
 // Function to get the color for the embed based on the day
 const getColorForDay = (day) => {
   const colors = [
-    0xFFD700, // Gold color
-    0x00FF00, // Green color
-    0x1E90FF, // Dodger Blue color
+    0xffd700, // Gold color
+    0x00ff00, // Green color
+    0x1e90ff, // Dodger Blue color
     // Add more colors for subsequent days if needed
   ];
 
@@ -90,12 +93,24 @@ const getColorForDay = (day) => {
 };
 
 // Set up the cron job to schedule the question and answer sending
-const questionSchedule = '00 09 * * *'; // 09:00 IST Asia/Kolkata
-const answerSchedule = '00 16 * * *'; // 04:00 IST Asia/Kolkata
+const questionSchedule = "15 17 * * *"; // 17:15 IST Asia/Kolkata
+const answerSchedule = "16 17 * * *"; // 17:16 IST Asia/Kolkata
 
 // Create cron job instances
-const sendQuestionsJob = new cron.CronJob(questionSchedule, sendQuestions, null, true, 'Asia/Kolkata');
-const sendAnswersJob = new cron.CronJob(answerSchedule, sendAnswers, null, true, 'Asia/Kolkata');
+const sendQuestionsJob = new cron.CronJob(
+  questionSchedule,
+  sendQuestions,
+  null,
+  true,
+  "Asia/Kolkata"
+);
+const sendAnswersJob = new cron.CronJob(
+  answerSchedule,
+  sendAnswers,
+  null,
+  true,
+  "Asia/Kolkata"
+);
 
 // Start the cron jobs
 sendQuestionsJob.start();
