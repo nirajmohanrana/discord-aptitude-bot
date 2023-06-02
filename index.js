a4a630f6e0dee462cd5bf21daae2ee7ba6dd80fb
Niraj Rana <71956273+nirajmohanrana@users.noreply.github.com>
2023-06-02 10:18:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const getColorForDay = (day) => {
 };
 
 // Set up the cron job to schedule the question and answer sending
-const questionSchedule = "15 10 * * *"; // 17:15 IST Asia/Kolkata
+const questionSchedule = "30 10 * * *"; // 17:15 IST Asia/Kolkata
 const answerSchedule = "00 16 * * *"; // 17:16 IST Asia/Kolkata
 
 // Create cron job instances
